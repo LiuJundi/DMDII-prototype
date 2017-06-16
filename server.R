4b2be5bd1d41f76c40c636a1e5ee9aaace45d1d7
feardameow <shwang1121@gmail.com>
2017-06-16 11:24:35 -0700

    Fixed Cal

--- server.R
+++ server.R
@@ -1,20 +1,26 @@
+library(shiny)
 library(shinydashboard)
 library(plotly)
 library(dplyr)
 library(stringdist)
 library(tm)
-library(makeR)
-library(chron)
+library(googleVis)
 
 setwd('D:\\Program File\\Git\\git_projects\\RA\\VizProto\\DMDII-prototype')
 df.cal <- read.csv('cal.csv', header=TRUE, colClasses = c('character','integer'))
+df.cal$Dates <- as.Date(df.cal$Dates)
+
 
 server <- function(input, output, session) {
-  output$calendar <- renderImage({
-    png(filename = "h1.png", 750, 750, res = 100)
-    calendarHeat(df.cal$Dates, df.cal$Value,varname = "Calendar of order commitment dates")
-    dev.off()
-    
-    list(src = 'h1.png')
+  output$calendar <- renderGvis({
+    gcal <- gvisCalendar(data=df.cal, datevar="Dates", numvar = "Value",  options=list(
+      title="Daily temperature in Cairo",
+      height=250,
+      calendar="{yearLabel: { fontName: 'Times-Roman',
+                               fontSize: 32, color: '#1A8763', bold: true},
+                               cellSize: 10,
+                               cellColor: { stroke: 'red', strokeOpacity: 0.2 },
+                               focusedCellColor: {stroke:'red'}}"))
+    return(gcal)
   })
 }
--- ui.R
+++ ui.R
@@ -1,34 +1,16 @@
+library(shiny)
 library(shinydashboard)
 library(plotly)
 library(dplyr)
 library(stringdist)
 library(tm)
+library(googleVis)
 
 #setwd('/Users/jundiliu/Desktop/DMDII')
+setwd('D:\\Program File\\Git\\git_projects\\RA\\VizProto\\DMDII-prototype')
 df.data <- read.csv('SalesOrdersLines-05222017.csv', header = TRUE, 
                     colClasses = c(rep("factor",4),"character","numeric","factor","character","factor","factor","character","numeric","factor","numeric"))
 df.data$required_date <- as.Date(df.data$required_date, format="%d-%b-%y")
-
-#Cleaning
-df.data$item_description <- tolower(df.data$item_description)
-df.data$drawing_description <- tolower(df.data$drawing_description)
-df.data$item_description <- removePunctuation(df.data$item_description, preserve_intra_word_dashes = TRUE)
-df.data$item_description <- gsub('\\S+([0-9])\\S+','',df.data$item_description)
-df.data$item_description <- sub("rev.*","",df.data$item_description)
-df.data$item_description <- removeNumbers(df.data$item_description)
-df.data$item_description <- removePunctuation(df.data$item_description)
-
-df.data$drawing_description <- removePunctuation(df.data$drawing_description, preserve_intra_word_dashes = TRUE)
-df.data$drawing_description <- gsub('\\S+([0-9])\\S+','',df.data$drawing_description)
-df.data$drawing_description <- sub("rev.*","",df.data$drawing_description)
-df.data$drawing_description <- removeNumbers(df.data$drawing_description)
-df.data$drawing_description <- removePunctuation(df.data$drawing_description)
-
-df.data$norm_descr <- ifelse(df.data$drawing_description == "",df.data$item_description, df.data$drawing_description)
-df.data$norm_descr <- trimws(df.data$norm_descr)
-
-df.data <- df.data %>% filter(!norm_descr == "")
-df.data$norm_descr <- sub("^(\\S*\\s+\\S+).*", "\\1",df.data$norm_descr)
 
 # Notification menu
 notifications <- dropdownMenu(type = "notifications",
@@ -50,17 +32,18 @@ sidebar <- dashboardSidebar(
   ),
   sliderInput(inputId = "quantity_ordered", "Quantity Ordered:", 1, 100, 50),
   selectizeInput(inputId = "part_id", label = "Part ID:", choices = unique(df.data$norm_descr), selected = FALSE, multiple = TRUE),
-  dateRangeInput(inputId = "date_range", label = "Date Range", start = NULL, end = NULL, min = NULL,
-                 max = NULL, format = "yyyy-mm-dd", startview = "month", weekstart = 0,
-                 language = "en", separator = " to ", width = NULL),
   selectInput(inputId = "customer_priority", label = "Customer Priority", choices = c("First", "Second", "Third"), selected = NULL, multiple = FALSE,
               selectize = TRUE, width = NULL, size = NULL)
 )
 
 # Body
 body <- dashboardBody(
-  box(title = "Histogram", status = "primary"),
-  box(title="Inputs")
+  fluidRow(
+    column(width=9,
+           box(width=NULL,title = "Order Calendar", status = "primary", htmlOutput("calendar"))
+           )
+    
+  )
 )
 
-ui <- dashboardPage(header, sidebar, body, skin = "purple")
+dashboardPage(header, sidebar, body, skin = "purple")
