b73c8a0d982ab20b2b1b1f251434c793b7ffcbc2
feardameow <shwang1121@gmail.com>
2017-06-16 01:34:39 -0700

    Added calendar

--- server.R
+++ server.R
@@ -1,8 +1,20 @@
 library(shinydashboard)
 library(plotly)
 library(dplyr)
+library(stringdist)
+library(tm)
+library(makeR)
+library(chron)
 
+setwd('D:\\Program File\\Git\\git_projects\\RA\\VizProto\\DMDII-prototype')
+df.cal <- read.csv('cal.csv', header=TRUE, colClasses = c('character','integer'))
 
 server <- function(input, output, session) {
-  
+  output$calendar <- renderImage({
+    png(filename = "h1.png", 750, 750, res = 100)
+    calendarHeat(df.cal$Dates, df.cal$Value,varname = "Calendar of order commitment dates")
+    dev.off()
+    
+    list(src = 'h1.png')
+  })
 }
