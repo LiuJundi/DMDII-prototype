d1f2a01f45d767eacf15944793afb355cfc7cffd
feardameow <shwang1121@gmail.com>
2017-06-16 19:46:48 -0700

    Fixed calendar size

--- server.R
+++ server.R
@@ -14,12 +14,12 @@ df.cal$Dates <- as.Date(df.cal$Dates)
 server <- function(input, output, session) {
   output$calendar <- renderGvis({
     gcal <- gvisCalendar(data=df.cal, datevar="Dates", numvar = "Value",  options=list(
-      title="Daily temperature in Cairo",
-      height=250,
+      title="Open Orders",
+      width='automatic',
+      length='automatic',
       calendar="{yearLabel: { fontName: 'Times-Roman',
-                               fontSize: 32, color: '#1A8763', bold: true},
-                               cellSize: 10,
-                               cellColor: { stroke: 'red', strokeOpacity: 0.2 },
+                               fontSize: 32, color: 'black', bold: true},
+                               cellSize: 15,
                                focusedCellColor: {stroke:'red'}}"))
     return(gcal)
   })
